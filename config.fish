5688d4b20ee44c99160c48d1cc64c1d4350323d6
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-12-20 00:56:54 +0100

    Disable local-vars-file check

--- config.fish
+++ config.fish
@@ -1,13 +1,14 @@
 umask 027
 set -x hostnamE (hostname)
 
-if string match -rq 'izbs*|iamcms-mze-*' $hostnamE
-    umask 0077
-	set local_vars (cat "$HOME"/.local_vars_pc)
-	for i in $local_vars
-	    set a (string split --max 1 --no-empty " " "$i")
-		eval set -x $a[1] $a[2]
-	end
-end
+#if string match -rq 'something*|some-thing-other-*' $hostnamE
+#    umask 0077
+#	set local_vars (cat "$HOME"/.local_vars_pc)
+#	for i in $local_vars
+#	    set a (string split --max 1 --no-empty " " "$i")
+#		eval set -x $a[1] $a[2]
+#	end
+#end
 
 set -x COMPLETION_HELPER_FUNCTIONS_DIR ~/.config/fish/completions
+# vim: set filetype=sh :
